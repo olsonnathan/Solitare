60ff53f8a921272fc931549fbb973f0e953881ac
Nathan Olson <olson.nathan@icloud.com>
2021-01-08 19:18:55 -0600

    Initial Commit

---
+++ Solitare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Solitare
+//
+//  Created by Nathan Olson on 1/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Solitare/SolitareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolitareApp.swift
+//  Solitare
+//
+//  Created by Nathan Olson on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SolitareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
